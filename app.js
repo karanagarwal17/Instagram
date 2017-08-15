4c00fcd6d8c002c9a5781059c4fea81c15a527bc
karanagarwal17 <karanagarwal1702@gmail.com>
2017-08-15 20:19:34 +0530

    implemented index router

--- app.js
+++ app.js
@@ -2,8 +2,16 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var logger = require('logger');
 var path = require('path');
+var webpack = require('webpack');
+var webpackMiddleware = require('webpack-dev-middleware');
+
+var webpackConfig = require('./webpack.config');
+var index  = require('./routers/index');
 
 var app = express();
+
+var compiler = webpack(webpackConfig);
+app.use(webpackMiddleware(compiler));
 
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine','jade');
@@ -12,7 +20,8 @@ app.set(express.static(path.join(__dirname,'public')));
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.use(logger('dev'));
+
+app.use('/',index);
 
 app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), () => {
